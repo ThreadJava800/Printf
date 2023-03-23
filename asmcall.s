1bcadbedbbcfa6b3120db14e6e7076364a1e34de
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-03-24 01:19:29 +0300

    Added c call from asm

--- asmcall.s
+++ asmcall.s
@@ -1,9 +1,30 @@
+extern kernel32connect
+
 section .text
 ; USE FOR CALL FROM ASSEMBLY
 global main
 
 main:
+    mov eax, 0
+    mov rdi, format
+    mov rsi, 120
+    mov rdx, 120
+    mov rcx, 120
+    mov r8,  text1
+    mov r9,  0x31
+
+    push 12
+    push 16
+
     call kernel32connect
+
+    pop r10
+    pop r10
+    
     ret
 
-extern kernel32connect
+section .data
+
+format: db "Test %d %x %o %s %c hello %d %d", 0x0A, 0x00
+
+text1:  db "R8Text"
